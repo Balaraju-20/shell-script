12a4d2d1511d40553679e89fc16bc4f78ebb1cb0
Balaraju-20 <kbraaju82@gmail.com>
2026-07-08 12:58:33 +0530

    cata

--- 09-special_var.sh
+++ 09-special_var.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "All variables passed to the script: $@"
-# echo "Number of variables passed: $#"
+echo "Number of variables passed: $#"
 # echo "Script name: $0"
 # echo "Current working directory: $PWD"
 # echo "Home directory of current user: $HOME"
