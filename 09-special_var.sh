36d5f776a081008a1b64b22fc38e0be99189eb13
Balaraju-20 <kbraaju82@gmail.com>
2026-07-08 13:02:53 +0530

    cata

--- 09-special_var.sh
+++ 09-special_var.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 echo "All variables passed to the script: $@"
-echo "my total no of days: $@"
 echo "Number of variables passed: $#"
-# echo "Script name: $0"
-# echo "Current working directory: $PWD"
-# echo "Home directory of current user: $HOME"
-# echo "PID of the script executing now: $$"
-# sleep 100 &
-# echo "PID of last background command: $!"
+echo "Script name: $0"
+echo "Current working directory: $PWD"
+echo "Home directory of current user: $HOME"
+echo "PID of the script executing now: $$"
+sleep 100 &
+echo "PID of last background command: $!"
